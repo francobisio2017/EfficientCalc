679705f9729436e74c95ab980789312e220c994d
francobisio2017 <francobisio092011@gmail.com>
2026-08-05 12:29:46 -0300

    Made code easier to read

--- calculadora.cpp
+++ calculadora.cpp
@@ -16,64 +16,64 @@ while (true) {
 cout << "Ingrese la operacion que desea realizar (+, -, *, /, ^, r, %)" << '\n';
 cin >> operador;
 
-if (operador == 'r') {
+    if (operador == 'r') {
+        cout << "Ingrese un numero" << '\n';
+        cin >> num1;
+
+        resultado = sqrt(num1);
+        cout << "El resultado de la raiz cuadrada es: " << resultado << '\n';
+        cout << '\n';
+        continue;
+    }
+    else if (operador == '%') {
+        cout << "Ingrese el porcentaje" << '\n';
+        cin >> num1;
+
+        cout << "Ingrese el numero" << '\n';
+        cin >> num2;
+
+        resultado = (num1 / 100) * num2;
+        cout << "El " << num1 << "% de " << num2 << " es: " << resultado << '\n';
+        continue;
+    }
+    else{
     cout << "Ingrese un numero" << '\n';
     cin >> num1;
 
-    resultado = sqrt(num1);
-    cout << "El resultado de la raiz cuadrada es: " << resultado << '\n';
-    cout << '\n';
-    continue;
-}
-else if (operador == '%') {
-    cout << "Ingrese el porcentaje" << '\n';
-    cin >> num1;
-
-    cout << "Ingrese el numero" << '\n';
+    cout << "Ingrese otro numero" << '\n';
     cin >> num2;
+    }
 
-    resultado = (num1 / 100) * num2;
-    cout << "El " << num1 << "% de " << num2 << " es: " << resultado << '\n';
-    continue;
-}
-else{
-cout << "Ingrese un numero" << '\n';
-cin >> num1;
-
-cout << "Ingrese otro numero" << '\n';
-cin >> num2;
-}
-
-if (num2 == 0 && operador == '/') {
-    cout << "Has activado la autodestrucción, corra o su PC explotará" << '\n';
-}
-else {
-switch (operador) {
-    case '+':
-        resultado = num1 + num2;
-        cout << "El resultado de la suma es: " << resultado << '\n';
-        break;
-    case '-':
-        resultado = num1 - num2;
-        cout << "El resultado de la resta es: " << resultado << '\n';
-        break;
-    case '*':
-        resultado = num1 * num2;
-        cout << "El resultado de la multiplicacion es: " << resultado << '\n';
-        break;
-    case '/':
-        resultado = num1 / num2;
-        cout << "El resultado de la division es: " << resultado << '\n';
-        break;
-    case '^':
-        resultado = pow(num1, num2);
-        cout << "El resultado de la potencia es: " << resultado << '\n';
-        break;
-    default:
-        cout << "por favor ingrese un operador valido (+,-,/,*,^,r,%)" << '\n';
-        break;
-}
-cout  << '\n';
-}
-}
-}
+    if (num2 == 0 && operador == '/') {
+        cout << "Has activado la autodestrucción, corra o su PC explotará" << '\n';
+    }
+    else {
+    switch (operador) {
+        case '+':
+            resultado = num1 + num2;
+            cout << "El resultado de la suma es: " << resultado << '\n';
+            break;
+        case '-':
+            resultado = num1 - num2;
+            cout << "El resultado de la resta es: " << resultado << '\n';
+            break;
+        case '*':
+            resultado = num1 * num2;
+            cout << "El resultado de la multiplicacion es: " << resultado << '\n';
+            break;
+        case '/':
+            resultado = num1 / num2;
+            cout << "El resultado de la division es: " << resultado << '\n';
+            break;
+        case '^':
+            resultado = pow(num1, num2);
+            cout << "El resultado de la potencia es: " << resultado << '\n';
+            break;
+        default:
+            cout << "por favor ingrese un operador valido (+,-,/,*,^,r,%)" << '\n';
+            break;
+    }
+    cout  << '\n';
+    }
+    }
+    }
